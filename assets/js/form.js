c8469f84235115d86248623a707b801dbaa4be26
iqbal <iqbal.aliandra@gmail.com>
2024-12-26 15:36:35 +0700

    try form.js

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,33 +1,29 @@
-(function () {
-    "use strict";
-    
-    let form = document.querySelectorAll(".php-email-form");
-    async function handleSubmit(event) {
-    event.preventDefault();
-    var data = new FormData(event.target);
-    fetch(event.target.action, {
-    method: form.method,
-    body: data,
-    headers: {
-        'Accept': 'application/json'
-    }
-    }).then(response => {
-    if (response.ok) {
-        thisForm.querySelector('.sent-message').classList.remove('d-block');
-        form.reset()
+var form = document.getElementById("my-form");
+async function handleSubmit(event) {
+event.preventDefault();
+var status = document.getElementById("my-form-status");
+var data = new FormData(event.target);
+fetch(event.target.action, {
+  method: form.method,
+  body: data,
+  headers: {
+    'Accept': 'application/json'
+}
+}).then(response => {
+  if (response.ok) {
+    status.innerHTML = "Thanks for your submission!";
+    form.reset()
+  } else {
+    response.json().then(data => {
+    if (Object.hasOwn(data, 'errors')) {
+      status.innerHTML = data["errors"].map(error => error["message"]).join(", ")
     } else {
-        response.json().then(data => {
-        if (Object.hasOwn(data, 'errors')) {
-        thisForm.querySelector('.error-message').innerHTML = error;
-        } else {
-            thisForm.querySelector('.error-message').classList.add('d-block');
-        }
-    })
+      status.innerHTML = "Oops! There was a problem submitting your form"
     }
-    }).catch(error => {
-        thisForm.querySelector('.error-message').classList.add('d-block');
-    });
-    }
-    form.addEventListener("submit", handleSubmit)
-
-})();
+  })
+}
+}).catch(error => {
+  status.innerHTML = "Oops! There was a problem submitting your form"
+});
+}
+form.addEventListener("submit", handleSubmit)
