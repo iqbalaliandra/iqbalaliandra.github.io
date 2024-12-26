5ac455e325e6180aa630228b8a7212d273cf0c8c
iqbal <iqbal.aliandra@gmail.com>
2024-12-26 16:45:07 +0700

    try form.js

--- assets/js/form.js
+++ assets/js/form.js
@@ -12,7 +12,7 @@ fetch(event.target.action, {
 }
 }).then(response => {
   if (response.ok) {
-    sentMessage.style.removeProperty(display);
+    sentMessage.style.removeProperty('display');
     console.log("response")
     form.reset()
   } else {
